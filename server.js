6d110e707a4af0be92537a1d705f828839698804
JulioPalacios97 <47432512+JulioPalacios97@users.noreply.github.com>
2019-04-11 13:21:50 -0500

    update server

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const app= express();
 const bodyParser=require('body-parser')
 const indexRouter=require("./rutas/routes");
 const companyRouter=require("./rutas/companies");
+const brandRouter=require("./rutas/brand")
 app.use(bodyParser());
 app.use("/public",express.static(__dirname+'/public/'));
 
 app.use("/rutas",indexRouter);
 app.use("/companias",companyRouter);
+app.use("/modelo",brandRouter);
 app.use("/*",(req,res)=>{res.sendFile(__dirname+'/public/index.html')});
 
 /*
