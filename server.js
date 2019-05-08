99b1085e98b0ee90d1129599140dc0ce105053bb
JulioPalacios97 <47432512+JulioPalacios97@users.noreply.github.com>
2019-05-08 16:53:27 -0500

    nueva actualizacion

--- server.js
+++ server.js
@@ -11,7 +11,25 @@ app.use("/public",express.static(__dirname+'/public/'));
 app.use("/rutas",indexRouter);
 app.use("/companias",companyRouter);
 app.use("/modelo",brandRouter);
+
+
+app.use("/editar",(req,res)=>{
+	res.sendFile(__dirname+"/public/enlaces/actualizar-buscar/buscar.html");
+});
+
+app.use("/eliminar",(req,res)=>{
+	res.sendFile(__dirname+"/public/enlaces/eliminar/eliminar.html");
+});
+
+
+app.use("/crear",(req,res)=>{
+	res.sendFile(__dirname+"/public/enlaces/crear/crear.html");
+});
+
+
+
 app.use("/*",(req,res)=>{res.sendFile(__dirname+'/public/index.html')});
+
 
 /*
 app.use("/*",(req,res)=>{
